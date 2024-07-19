649277bb21a3242cfefc7193e08779247089734a
Ajayduddi <ajayprofessional19@gmail.com>
2024-07-19 16:39:17 +0530

    update cookie policy

--- backend/src/app.mjs
+++ backend/src/app.mjs
@@ -32,8 +32,8 @@ app.use(session({
   cookie: {
     maxAge: 1 * 24 * 60 * 60 * 1000,// 1 day
     httpOnly: true,
-    // secure: true, // set to true if you only serve the app over https
-    sameSite: 'strict',
+    secure: false, // set to true if you only serve the app over https
+    sameSite: 'none',
   },
   store: mongoStore.create({
     client: mongoose.connection.getClient(),
