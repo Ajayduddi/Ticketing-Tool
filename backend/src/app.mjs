d48d2fcc52261639b9f950941d0b7fc7322334e7
Ajayduddi <ajayprofessional19@gmail.com>
2024-07-29 12:42:53 +0530

    update header

--- backend/src/app.mjs
+++ backend/src/app.mjs
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://ajayduddi.github.io');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept','Set-Cookie');
   next();
 });
 
