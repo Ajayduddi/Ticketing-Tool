b0039f6eced6d741324a3fbac7107e5df7716e36
Ajayduddi <ajayprofessional19@gmail.com>
2024-07-29 14:49:51 +0530

    Revert "comments"
    This reverts commit 731299f5b9d36905ddff6907001d5a92eed51787.

--- backend/src/app.mjs
+++ backend/src/app.mjs
@@ -55,8 +55,8 @@ app.use(session({
   cookie: {
     maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
     httpOnly: true,
-    // sameSite: 'None', // Ensure cookies are sent with cross-site requests
-    // secure: false, // Set to true if you only serve the app over HTTPS
+    sameSite: 'None', // Ensure cookies are sent with cross-site requests
+    secure: false, // Set to true if you only serve the app over HTTPS
     // domain: ".github.io", // Domain without the protocol
     Path: "/",
   },
