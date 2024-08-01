478648dc6b036019991c0182ab23824003f612a5
Ajayduddi <ajayprofessional19@gmail.com>
2024-08-01 22:35:13 +0530

    add set cookie and check cookie

--- backend/src/routes/index.mjs
+++ backend/src/routes/index.mjs
@@ -10,6 +10,19 @@ router.get('/', (req, res) => {
   res.send('Welcome to the Ticketing tool API!!');
 });
 
+router.get("/set-cookie", (req, res) => {
+  res.cookie("thirdPartyTest", "enabled", {
+    maxAge: 900000,
+    httpOnly: false,
+  });
+  res.send("Cookie set");
+});
+
+router.get("/check-cookie", (req, res) => {
+  const cookie = req.cookies["thirdPartyTest"];
+  res.send(cookie ? "enabled" : "disabled");
+});
+
 router.use(employeeRouter);
 router.use(deptRoputer);
 router.use(categoryRouter);
