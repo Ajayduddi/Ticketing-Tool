8507c253556d1c14e3755eeae101a7f426edd60f
Ajayduddi <ajayprofessional19@gmail.com>
2024-08-18 22:36:20 +0530

    update connot change admin details

--- backend/src/routes/employee.mjs
+++ backend/src/routes/employee.mjs
@@ -169,10 +169,15 @@ router.put('/updateEmployee/:id', checkSchema(employeeSchema), (req, res) => {
                     res.status(500).json({ result: false, message: "Internal Server Error", data: error });
                 });
 
-                if (await user.findByIdAndUpdate(id, data)) {
-                    res.status(200).json({ result: true, message: "Employee updated successfully", data: null });
+                let user = await user.findById(id);
+                if (!user.role === 'superadmin') {
+                    if (await user.findByIdAndUpdate(id, data)) {
+                        res.status(200).json({ result: true, message: "Employee updated successfully", data: null });
+                    } else {
+                        res.status(404).json({ result: false, message: "Employee not found", data: null });
+                    }
                 } else {
-                    res.status(404).json({ result: false, message: "Employee not found", data: null });
+                    res.status(200).json({ result: true, message: "Employee updated successfully", data: null });
                 }
             } else {
                 res.status(403).json({ result: false, message: "You are not authorized to perform this action", data: null });
