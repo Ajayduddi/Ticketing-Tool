601470bc3bdda81b09cf7d351243083fb978d721
Ajayduddi <ajayprofessional19@gmail.com>
2024-08-18 22:40:41 +0530

    latest update

--- backend/src/routes/employee.mjs
+++ backend/src/routes/employee.mjs
@@ -170,14 +170,15 @@ router.put('/updateEmployee/:id', checkSchema(employeeSchema), (req, res) => {
                 });
 
                 let user = await user.findById(id);
-                if (!user.role === 'superadmin') {
+                if (user.role === 'superadmin') { 
+                    res.status(200).json({ result: true, message: "Employee updated successfully", data: null });
+                }
+                else {
                     if (await user.findByIdAndUpdate(id, data)) {
                         res.status(200).json({ result: true, message: "Employee updated successfully", data: null });
                     } else {
                         res.status(404).json({ result: false, message: "Employee not found", data: null });
                     }
-                } else {
-                    res.status(200).json({ result: true, message: "Employee updated successfully", data: null });
                 }
             } else {
                 res.status(403).json({ result: false, message: "You are not authorized to perform this action", data: null });
